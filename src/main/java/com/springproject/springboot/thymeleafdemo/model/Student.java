95d94ae205e1bf697bef639da6d8b33067bf8554
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-04-03 23:28:49 -0500

    Tried to add some values directly inside application.properties

--- src/main/java/com/springproject/springboot/thymeleafdemo/model/Student.java
+++ src/main/java/com/springproject/springboot/thymeleafdemo/model/Student.java
@@ -4,6 +4,8 @@ public class Student {
 
     private String firstName;
     private String lastName;
+    private String country;
+    private String favoriteLanguage;
 
     public Student() {
 
@@ -23,5 +25,21 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
     }
 }
